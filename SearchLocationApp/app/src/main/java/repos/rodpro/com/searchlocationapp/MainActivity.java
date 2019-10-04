1777c61418e4740265b567aaadfc22b88733b1a9
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-10-04 09:29:53 -0500

    Create Android Proyect

---
+++ SearchLocationApp/app/src/main/java/repos/rodpro/com/searchlocationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package repos.rodpro.com.searchlocationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
